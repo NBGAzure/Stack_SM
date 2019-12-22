4b1552f887aefee14c6a9fee0e7af9f659a0e2a6
VD <vickydesai8002@gmail.com>
2019-12-22 19:19:45 +0530

    dashboard error

--- application/modules/admin/controllers/Home.php
+++ application/modules/admin/controllers/Home.php
@@ -8,9 +8,7 @@ class Home extends MY_Controller {
         
     public function index()
     {  
-
-        $this->load->model('Admin_model'); 
-        $this->load->view('admin/header');
+		$this->load->view('admin/header');
         $this->load->view('admin/nav');
         $this->load->view('dashboard');
         $this->load->view('admin/footer');
