1081f8dd8aeb74eb0417915f8e0085307f0938f2
VD <vickydesai8002@gmail.com>
2019-12-25 18:23:29 +0530

    pstock

--- application/models/Productstock_model.php
+++ application/models/Productstock_model.php
@@ -109,7 +109,7 @@ Class Productstock_model extends CI_Model
             $this->db->join('product_stock as ps', 'ps.dept_id = pro.dept_id and `ps`.`product_id` = `pro`.`product_id`','left');
             $this->db->where('pro.dept_id',$did);
             $this->db->where('ps.uid',$uid);
-            if(date('ps.create_date') = date('Y-m-d')){
+            if(date('ps.create_date') == date('Y-m-d')){
                 $this->db->where('date(ps.create_date)',date('Y-m-d'));
             }
             elseif(date('ps.create_date')!= date('Y-m-d') && date('ps.create_date') == date('Y-m-d', strtotime(' -1 day'))){
