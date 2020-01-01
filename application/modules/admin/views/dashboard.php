e825dbf1d445afa3a119f1221d1885ff904cabce
VD <vickydesai8002@gmail.com>
2020-01-01 08:54:22 +0530

    dash

--- application/modules/admin/views/dashboard.php
+++ application/modules/admin/views/dashboard.php
@@ -1,5 +1,18 @@
  <section id="content_wrapper" style="margin-top: 67px;">
-
+    <style type="text/css">
+        li.dash_li{
+            padding: 10px;
+            background: #2a2f43;
+            color: black !important;
+            list-style-type: none;
+            margin-bottom: 20px;
+            width: 85%;
+            text-align: center;
+        }
+        .sec{
+            background: #e5eaee !important;
+        }
+    </style>
 
         <!-- -------------- Topbar -------------- -->
         <header id="topbar" class="alt">
@@ -18,9 +31,23 @@
                     </li>
                     <li class="breadcrumb-current-item">Dashboard</li>
                 </ol>
+
             </div>
-            
+            <br/>
         </header>
+
+        <div id="topbar" class="alt sec">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-icon">
+                    <a>
+                        <span class="fa fa-power-off"></span>
+                    </a>
+                </li>
+                <li class="breadcrumb-link">
+                    <a href="<?php echo base_url();?>admin/Home/logout">Logout</a>
+                </li>
+            </ol>
+        </div>
         <!-- -------------- /Topbar -------------- -->
 
         <!-- -------------- Content -------------- -->
@@ -31,17 +58,50 @@
 
                 <!-- -------------- Quick Links -------------- -->
                 <div class="row">
-                    <!-- <div class="col-sm-6 col-xl-6">
+                    <div class="col-sm-6 col-xl-6">
                         <div class="panel panel-tile">
                             <div class="panel-body">
                                 <div class="row pv10">
-                                    <div class="col-xs-5 ph10"><img src="<?php echo base_url()?>assets/img/pages/clipart0.png"
-                                                                    class="img-responsive mauto" alt=""/></div>
-                                    <div class="col-xs-7 pl5">
-                                        <h6 class="text-muted">PRODUCTS</h6>
+                                    <!-- <div class="col-xs-5 ph10"><img src="<?php echo base_url()?>assets/img/pages/clipart0.png"
+                                                                    class="img-responsive mauto" alt=""/></div> -->
+                                    <div class="col-xs-2 pl5"></div>
+                                    <div class="col-xs-10 pl5">
+                                        <h6 class="text-muted">Departments</h6>
 
-                                        <h2 class="fs50 mt5 mbn"></h2>
+                                        <!-- <h2 class="fs50 mt5 mbn"><?php// echo $total_artists; ?></h2> -->
+                                        <?php if($_SESSION['username']!='superadmin') {  ?>
+                                            <?php //if(current_url() == site_url('admin/Productstock/productstock_list')) { $class = 'active';} else { $class =''; }  ?>
+                                            <!-- <li class="<?php print $class; ?>">
+                                               <a href="<?php echo site_url('admin/Productstock/productstock_list');?>">
+                                                    <span class="fa fa-desktop"></span>
+                                                    <span class="sidebar-title">Product Stock</span>
+                                                </a>
+                                            </li> -->
+                                            <?php 
+                                            $uid =$this->session->userdata['uid'];
+                                            $query = $this->db->query("SELECT * from department WHERE `visible_for` = ".$uid." ");
+                                            foreach ($query->result() as $val)  { ?>
+                                            <li class="dash_li">
+                                               <a href="<?php echo base_url()?>admin/Productstock/departmentproduct/<?php echo $val->dept_id; ?>">
+                                                    <span class="sidebar-title"><?php echo $val->department_name;?></span>
+                                                </a>
+                                            </li>
+                                            <?php  } ?>
+                                        <?php  } ?>
+
+                                        <?php if($_SESSION['username']=='superadmin') {  ?>
+                                           
+                                            <?php 
+                                            $uid =$this->session->userdata['uid'];
+                                            $query = $this->db->query("SELECT * from department");
+                                            foreach ($query->result() as $val)  { ?>
+                                            <li class="dash_li">
+                                               <a><span class="sidebar-title"><?php echo $val->department_name;?></span></a>
+                                            </li>
+                                            <?php  } ?> 
+                                        <?php  } ?>
                                     </div>
+                                    <div class="col-xs-2 pl5"></div>
                                 </div>
                             </div>
                         </div>
@@ -51,16 +111,39 @@
                         <div class="panel panel-tile">
                             <div class="panel-body">
                                 <div class="row pv10">
-                                    <div class="col-xs-5 ph10"><img src="<?php echo base_url()?>assets/img/pages/clipart2.png" class="img-responsive mauto" alt=""/></div>
-                                    <div class="col-xs-7 pl5">
-                                        <h6 class="text-muted">DEPARTMENTS</h6>
-
-                                        <h2 class="fs50 mt5 mbn"></h2>
+                                    <div class="col-xs-2 p15">
+                                        <!-- <img src="<?php echo base_url()?>assets/img/pages/clipart2.png" class="img-responsive mauto" alt=""/> -->
                                     </div>
+                                    <div class="col-xs-10 pl5">
+                                        <h6 class="text-muted">REQUEST</h6>
+
+                                        <h2 class="fs50 mt5 mbn"><?php //echo $total_hosts; ?></h2>
+
+                                         <?php if($_SESSION['username']!='superadmin') {  ?>
+                                            
+                                            <li class="dash_li">
+                                               <a href="<?php echo base_url()?>admin/Request/request_list">
+                                                    <span class="sidebar-title">Request</span>
+                                                </a>
+                                            </li>   
+                                        <?php } ?>
+
+                                         <?php if($_SESSION['username']=='superadmin') {  ?>
+                                            
+                                            <li class="dash_li">
+                                               <a href="<?php echo base_url()?>admin/Request/adminrequest_list">
+                                                    <span class="sidebar-title">Request</span>
+                                                </a>
+                                            </li>   
+                                        <?php } ?>
+
+                                    </div>
+
+                                    <div class="col-xs-2 p15"></div>
                                 </div>
                             </div>
                         </div>
-                    </div> -->
+                    </div>
                     
                 </div>
 
