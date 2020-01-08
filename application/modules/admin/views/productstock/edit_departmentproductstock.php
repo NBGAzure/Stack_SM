39b20ac46e5b5741e1b5f3917aea88ab763ef84e
VD <vickydesai8002@gmail.com>
2020-01-08 07:15:01 +0530

    ps

--- application/modules/admin/views/productstock/edit_departmentproductstock.php
+++ application/modules/admin/views/productstock/edit_departmentproductstock.php
@@ -86,7 +86,8 @@
                                                 <td>
                                                     <!-- <input type="hidden" id="" name="check_val[]" value="0" /> -->
                                                     <!-- <input id='checkItemHidden[]' type='hidden' value='0' name='check_val[]'> -->
-                                                    <input type="checkbox" id="checkItem[]" name="check_val[<?php echo $val->product_id;?>][]" value="1" <?php if(isset($val->check_val) && $val->check_val==1){echo "checked";}else{echo "";}?>>
+                                                    <input type="checkbox" id="checkItem[]" name="check_val[<?php echo $val->product_id;?>][]" value="<?php if(isset($val->check_val)){echo $val->check_val;}else{
+                                                  echo "";}?>">
                                                 </td>
                                             </tr>
                                         <?php 
