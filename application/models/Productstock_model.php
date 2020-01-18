977f16381542a2ce8be5a9765110b3c03e7f6bad
VickyDesai <vickydesai8002@gmail.com>
2020-01-18 11:21:33 +1000

    up

--- application/models/Productstock_model.php
+++ application/models/Productstock_model.php
@@ -318,7 +318,7 @@ Class Productstock_model extends CI_Model
         );
         $this->load->library('email', $config);
         $this->email->set_newline("\r\n");
-        $this->email->from('daily-report@resnbot.net');
+        $this->email->from('vicky@resnbot.net');
         $this->email->to($email);
         $this->email->subject($subject);
         $this->email->message($message);
--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -437,7 +437,7 @@ Class Report_model extends CI_Model
         );
         $this->load->library('email', $config);
         $this->email->set_newline("\r\n");
-        $this->email->from('daily-report@resnbot.net');
+        $this->email->from('vicky@resnbot.net');
         $this->email->to($email);
         $this->email->subject($subject);
         $this->email->message($message);
